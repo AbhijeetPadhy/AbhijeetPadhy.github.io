8adbb25bbf75045334c516bf36546bb20eada2c5
Abhijeet Padhy <abhijeet.padhy@gmail.com>
2017-04-30 23:34:01 +0530

    Changed Jumbotron

--- js/myJs.js
+++ js/myJs.js
@@ -2,6 +2,11 @@ function load(){
 	var h = window.innerHeight;
 	//console.log(document.getElementById("jumbo").style.minHeight);
 	document.getElementById("jumbo").style.minHeight = h+"px";
+	document.getElementById("GI").style.minHeight = h+"px";
+	document.getElementById("EQ").style.minHeight = h+"px";
+	document.getElementById("AP").style.minHeight = h+"px";
+	document.getElementById("SP").style.minHeight = h+"px";
+	document.getElementById("SC").style.minHeight = h+"px";
 	//document.getElementById('jumbo').style.min-height=h+"px";
 	//console.log(document.getElementById('jumbo').style.height);
 }
