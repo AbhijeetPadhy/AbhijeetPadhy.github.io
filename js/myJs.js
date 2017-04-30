c7f9bdb922d7d268a9ff724f363563c27a5051bb
Abhijeet Padhy <abhijeet.padhy@gmail.com>
2017-04-30 22:12:04 +0530

    Scroll feature

---
+++ js/myJs.js
@@ -0,0 +1,7 @@
+function load(){
+	var h = window.innerHeight;
+	//console.log(document.getElementById("jumbo").style.minHeight);
+	document.getElementById("jumbo").style.minHeight = h+"px";
+	//document.getElementById('jumbo').style.min-height=h+"px";
+	//console.log(document.getElementById('jumbo').style.height);
+}
